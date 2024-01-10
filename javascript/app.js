c793805acfffb10f7c956bc1132f29937526bde8
husanjonmusayev <husanjonm279@gmail.com>
2024-01-11 00:52:33 +0500

    uy ishi

---
+++ javascript/app.js
@@ -0,0 +1,9 @@
+let search = document.getElementById('search')
+let submit = document.getElementById('submit')
+let type = document.getElementById('type')
+let color = document.getElementById('color')
+let minSum = document.getElementById('minSum')
+let maxSum = document.getElementById('maxSum')
+
+let data = JSON.parse(localStorage.getItem('card'))
+
---
+++ javascript/index.js
@@ -0,0 +1,43 @@
+let type = document.querySelector('#type')
+let color = document.getElementById('color')
+let sum = document.getElementById('sum')
+let pres = document.getElementById('pres')
+let desc = document.getElementById('desc')
+let img = document.querySelector('.img')
+let file = document.getElementById('file')
+let btn = document.getElementById('btn')
+let form = document.getElementById('form')
+
+img.addEventListener('click', function(){
+    file.click()
+})
+
+function validate(){
+    if(!file.value){
+        img.style.borderColor = 'red'
+        return false
+    } else{
+    img.style.borderColor = 'teal'
+    }
+    return true
+}
+let data = JSON.parse(localStorage.getItem('card'))
+?JSON.parse(localStorage.getItem('card'))
+:[]
+
+btn && btn.addEventListener('click', function(){
+validate()
+if(validate()){
+    let list = {
+        file : URL.createObjectURL(file.files[0]),
+        type : type.value,
+        color : color.value,
+        sum : sum.value,
+        pres : pres.value,
+        desc : desc.value,
+    }
+    data.push(list)
+}
+console.log(data);
+localStorage.setItem('card', JSON.stringify(data))
+})
